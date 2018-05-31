6ef3cf7428954210a9ce6051567354b85bc059cf
adavies312 <a.m.davies1020@gmail.com>
2018-05-31 16:09:34 -0500

    Eat Da Burger

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,9 @@
 var mysql = require('mysql');
 
-
+if (process.env.JAWSDB_URL) {
+    // If on Heroku and JawsDB is present, use this
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,7 +12,7 @@ var connection = mysql.createConnection({
     database: "burgers_db",
     socketPath: "/Applications/MAMP/tmp/mysql/mysql.sock"
   });
-  
+}
   connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
